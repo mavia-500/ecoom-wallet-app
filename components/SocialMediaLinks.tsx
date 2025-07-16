6aca3b133a7e960abad5eff6406a3d636f1cd550
mavia-500 <maviasajjadabbasi@gmail.com>
2025-07-16 18:16:52 +0500

    add more product in card holder

--- components/SocialMediaLinks.tsx
+++ components/SocialMediaLinks.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { BsFillThreadsFill } from "react-icons/bs";
@@ -9,15 +9,31 @@ import { FaTiktok } from "react-icons/fa6";
 
 const SocialMediaLinks = () => {
   return (
-    <div className='flex space-x-5 m-10'>
-        <Link href={'https://www.facebook.com/profile.php?id=61573823107878'}><FaFacebook /></Link>
-        <Link href={'https://www.instagram.com/hilyah965?igsh=NnpjcXAxN29ldHVi'}><FaInstagram /></Link>
-        <Link href={''}><BsFillThreadsFill /></Link>
-        <Link href={''}><FaYoutube /></Link>
-        <Link href={'https://wa.me/923285478737?text=Hello%2C%20I%20am%20interested%20in%20your%20products'}><IoLogoWhatsapp /></Link>
-        <Link href={''}><FaTiktok /></Link>
+    <div className="flex space-x-5 m-10">
+      <Link href={"https://www.facebook.com/profile.php?id=61573823107878"}>
+        <FaFacebook />
+      </Link>
+      <Link href={"https://www.instagram.com/hilyah965?igsh=NnpjcXAxN29ldHVi"}>
+        <FaInstagram />
+      </Link>
+      <Link href={""}>
+        <BsFillThreadsFill />
+      </Link>
+      <Link href={""}>
+        <FaYoutube />
+      </Link>
+      <Link
+        href={
+          "https://wa.me/923285478737?text=Hello%2C%20I%20am%20interested%20in%20your%20products"
+        }
+      >
+        <IoLogoWhatsapp />
+      </Link>
+      <Link href={""}>
+        <FaTiktok />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMediaLinks
+export default SocialMediaLinks;
--- data/carholderProducts.ts
+++ data/carholderProducts.ts
@@ -1,51 +1,71 @@
 interface Product {
-    id: number;
-    title: string;
-    description: string;
-    image: string[];
-    color: string;
-    price: number;
-    discountedPrice: number;
-  }
-  
-  const cardholderProducts: Product[] = [
-    // {
-    //   id: 1,
-    //   title: "Leather Stylish Card holder",
-    //   description: "Card Compartment. Cash Compartment. 100% Leather. Top to Bottom Leather no cloth stuff is use",
-    //   image: ["https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928596/5_afycef.webp",'https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928596/4_xjiwgb.webp'],
-    //   color: "Brown",
-    //   price: 1600,
-    //   discountedPrice: 200,
-    // },
-    // {
-    //   id: 2,
-    //   title: "Slim Minimalist Card Holder",
-    //   description: "Sleek design with quick-access card slot and cash compartment.",
-    //   image: ["https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928600/8_ax7mpk.webp",'https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928601/9_s9fo7r.webp','https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928598/7_zdqhkf.webp','https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928597/6_qjkuae.webp'],
-    //   color: "Black",
-    //   price: 1600,
-    //   discountedPrice:100 ,
-    // },
-    {
-      id: 3,
-      title: "Vintage Canvas Card Holder",
-      description: "Durable Leather material with a retro aesthetic and sturdy stitching. Elegeant to Use. Quality Warranty Card slots. Cash Compartment. Id Compartment",
-      image:[ "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750938587/IMG_20250427_224034_727_zhyowp.jpg",'https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750938586/IMG_20250427_224136_480_eoxan2.jpg','https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750938585/IMG_20250427_224217_387_fbkf7c.jpg'],
-      color: "Black",
-      price: 1400,
-      discountedPrice: 200,
-    },
-    {
-      id: 4,
-      title: "Pop Up Cardholder",
-      description: "Pop Up for Card. made of Leather. 1 zipper Comaprtment. Cash Compartment. Easy to Carry",
-      image: ["https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928602/popup1.._wzxsh4.webp",'https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928605/poup1._e3ffzw.jpg','https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928606/poup1_ywmdn6.jpg','https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928604/poup_ep19or.jpg'],
-      color: "Navy",
-      price: 2400,
-      discountedPrice: 400,
-    },
-    
-  ];
-  
-  export default cardholderProducts;
+  id: number;
+  title: string;
+  description: string;
+  image: string[];
+  color: string;
+  price: number;
+  discountedPrice: number;
+}
+
+const cardholderProducts: Product[] = [
+  {
+    id: 1,
+    title: "Leather Stylish Card holder",
+    description:
+      "Card Compartment. Cash Compartment. 100% Leather. Top to Bottom Leather no cloth stuff is use",
+    image: [
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928596/5_afycef.webp",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928596/4_xjiwgb.webp",
+    ],
+    color: "Brown",
+    price: 1600,
+    discountedPrice: 200,
+  },
+  {
+    id: 2,
+    title: "Slim Minimalist Card Holder",
+    description:
+      "Sleek design with quick-access card slot and cash compartment.",
+    image: [
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928600/8_ax7mpk.webp",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928601/9_s9fo7r.webp",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928598/7_zdqhkf.webp",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928597/6_qjkuae.webp",
+    ],
+    color: "Black",
+    price: 1600,
+    discountedPrice: 100,
+  },
+  {
+    id: 3,
+    title: "Vintage Canvas Card Holder",
+    description:
+      "Durable Leather material with a retro aesthetic and sturdy stitching. Elegeant to Use. Quality Warranty Card slots. Cash Compartment. Id Compartment",
+    image: [
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750938587/IMG_20250427_224034_727_zhyowp.jpg",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750938586/IMG_20250427_224136_480_eoxan2.jpg",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750938585/IMG_20250427_224217_387_fbkf7c.jpg",
+    ],
+    color: "Black",
+    price: 1400,
+    discountedPrice: 200,
+  },
+  {
+    id: 4,
+    title: "Pop Up Cardholder",
+    description:
+      "Pop Up for Card. made of Leather. 1 zipper Comaprtment. Cash Compartment. Easy to Carry",
+    image: [
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928602/popup1.._wzxsh4.webp",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928605/poup1._e3ffzw.jpg",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928606/poup1_ywmdn6.jpg",
+      "https://res.cloudinary.com/dkbyjjnw5/image/upload/v1750928604/poup_ep19or.jpg",
+    ],
+    color: "Navy",
+    price: 2400,
+    discountedPrice: 400,
+  },
+];
+
+export default cardholderProducts;
